93671bd7045c87dc2fe3a26cebefc48fb3ad05c7
mirror-kt <me@mirror-kt.dev>
2021-01-24 02:41:27 +0900

    👷 Add detekt for static code analytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,26 @@
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
+plugins {
+    id("io.gitlab.arturbosch.detekt") version "1.15.0" apply false
+}
+
 group = "dev.mirror-kt"
 version = "0.1.0"
 
 allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
     repositories {
         mavenCentral()
+        jcenter()
+    }
+
+    extensions.configure<DetektExtension>("detekt") {
+        reports {
+            xml.enabled = true
+
+            txt.enabled = false
+            html.enabled = false
+        }
     }
 }
